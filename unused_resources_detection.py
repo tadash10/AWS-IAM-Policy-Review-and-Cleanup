3d8a7284fe9581c822a6b467e0b3462337c50cf4
T1 <126980610+tadash10@users.noreply.github.com>
2023-09-13 00:49:17 +0000

    Update unused_resources_detection.py

--- unused_resources_detection.py
+++ unused_resources_detection.py
@@ -1,5 +1,31 @@
 import boto3
 
 def detect_unused_resources(roles, users):
-    # Implement logic to detect unused IAM roles and users
-    pass
+    cloudtrail_client = boto3.client('cloudtrail')
+
+    # Retrieve CloudTrail logs to check for resource usage
+    try:
+        response = cloudtrail_client.lookup_events(LookupAttributes=[{'AttributeKey': 'ResourceName', 'AttributeValue': 'arn:aws:iam::account-id:role/role-name'}])
+        events = response['Events']
+    except Exception as e:
+        print(f"Error retrieving CloudTrail logs: {e}")
+        return
+
+    # Implement logic to detect unused IAM roles and users based on CloudTrail events
+    for role in roles:
+        role_name = role['RoleName']
+        role_arn = role['Arn']
+        if not any(event['Username'] == role_name for event in events):
+            print(f"Role {role_name} ({role_arn}) is unused.")
+
+    for user in users:
+        user_name = user['UserName']
+        user_arn = user['Arn']
+        if not any(event['Username'] == user_name for event in events):
+            print(f"User {user_name} ({user_arn}) is unused.")
+
+if __name__ == "__main__":
+    # For testing, provide lists of roles and users to check for unused resources
+    roles_to_check = []  # Replace with actual roles
+    users_to_check = []  # Replace with actual users
+    detect_unused_resources(roles_to_check, users_to_check)

1dc1eaa4ebbada8b26303639ca3600ca1fccf443
T1 <126980610+tadash10@users.noreply.github.com>
2023-09-13 00:48:58 +0000

    Update iam_policy_analysis.py

--- iam_policy_analysis.py
+++ iam_policy_analysis.py
@@ -1,5 +1,37 @@
 import boto3
 
 def analyze_iam_policies(policies):
-    # Implement policy analysis logic here
-    pass
+    iam_client = boto3.client('iam')
+
+    for policy in policies:
+        policy_name = policy['PolicyName']
+        policy_arn = policy['Arn']
+
+        # Get the policy version
+        try:
+            response = iam_client.get_policy(PolicyArn=policy_arn)
+            policy_version = response['Policy']['DefaultVersionId']
+        except Exception as e:
+            print(f"Error getting policy version for {policy_name}: {e}")
+            continue
+
+        # Get the policy document for the default version
+        try:
+            response = iam_client.get_policy_version(PolicyArn=policy_arn, VersionId=policy_version)
+            policy_document = response['PolicyVersion']['Document']
+        except Exception as e:
+            print(f"Error getting policy document for {policy_name}: {e}")
+            continue
+
+        # Implement policy analysis logic here based on policy_document
+        # Example: Check for overly permissive actions or conditions
+
+        # Print the analyzed results
+        print(f"Policy: {policy_name} ({policy_arn})")
+        print("Analysis Result:")
+        # Print analysis results here
+
+if __name__ == "__main__":
+    # For testing, provide a list of policies to analyze
+    policies_to_analyze = []  # Replace with actual policies
+    analyze_iam_policies(policies_to_analyze)
